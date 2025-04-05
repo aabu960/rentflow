aded9b5297f685544c4bd7cad82de8c96b6644dc
aabu960 <aabu960@yahoo.com>
2025-04-05 10:22:41 +0000

    intergrated backend to frontend properties page

--- frontend/src/pages/properties.jsx
+++ frontend/src/pages/properties.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-const API_URL = "https://rentflow-l80j.onrender.com/properties";
+const API_URL = " https://rentflow-backend.onrender.com/properties";
 
 const Properties = () => {
   const [properties, setProperties] = useState([]);
